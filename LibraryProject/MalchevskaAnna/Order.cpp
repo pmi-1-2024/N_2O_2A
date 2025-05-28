bdcb17e9463e9a1a3fe1a7c935718b1b8733ac2e
Anna4511 <malchevskanna@gmail.com>
2025-05-28 21:10:57 +0300

    Final upgrade

--- LibraryProject/MalchevskaAnna/Order.cpp
+++ LibraryProject/MalchevskaAnna/Order.cpp
@@ -1,50 +1,76 @@
 #include "Order.h"
+#include <stdexcept> 
+#include <limits>    
 
-Order::Order() : id(int()), bookID(int()), userID(""), startDay("") {}
+Order::Order() : id(0), bookID(0), libId(""), userID(""), startDay("") {}
 
-Order::Order(int i, int bid, string uid, string sd): id(i), bookID(bid), userID(uid), startDay(sd) {}
+Order::Order(int i, int bid, string lbId, string uid, string sd)
+    : id(i), bookID(bid), libId(lbId), userID(uid), startDay(sd) {}
 
-int Order::getId() const
+int Order::getId() const { return id; }
+
+int Order::getBookID() const { return bookID; }
+
+int Order::getOrderId() const { return id; }
+
+string Order::getUserID() const { return userID; }
+
+string Order::getLibId() const { return libId; }
+
+void Order::SetUserID(string userid)
 {
-	return id;
+    if (userid.empty()) {
+        throw invalid_argument("User ID cannot be empty");
+    }
+    userID = userid;
 }
 
-int Order::getBookID() const
+void Order::SetLibID(string libID)
 {
-	return bookID;
-}
-
-string Order::getUserID() const
-{
-	return userID;
+    if (libID.empty()) {
+        throw invalid_argument("Library ID cannot be empty");
+    }
+    libId = libID;
 }
 
 void Order::ConsolePrint() const
 {
-	cout << "\nOrder ID: " << id << ", Book ID: " << bookID;
-	cout << "\nUser ID: " << userID << ", Start Date: " << startDay;
+    cout << "\nOrder ID: " << id << "; Library Id: " << libId << ", Book ID: " << bookID;
+    cout << "\nUser ID: " << userID << ", Start Date: " << startDay;
 }
 
 void Order::ConsoleInput()
 {
     cout << "\nEnter Order ID: ";
     cin >> id;
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        throw invalid_argument("Invalid Order ID input");
+    }
     cout << "\nEnter Book ID: ";
     cin >> bookID;
-    cout << "\nEnter User ID: ";
-    cin >> userID;
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        throw invalid_argument("Invalid Book ID input");
+    }
+    cin.ignore();
     cout << "\nEnter Start Date: ";
     getline(cin, startDay);
+    if (startDay.empty()) {
+        throw invalid_argument("Start date cannot be empty");
+    }
 }
 
 istream& operator>>(istream& in, Order& o)
 {
-    in >> o.id >> o.bookID >> o.userID, in.ignore(), getline(in, o.startDay);
+    in >> o.id >> o.bookID >> o.libId >> o.userID, in.ignore(), getline(in, o.startDay, ';');
     return in;
 }
 
 ostream& operator<<(ostream& out, const Order& o)
 {
-    out << o.id << " " << o.bookID << " " << o.userID << " " << o.startDay;
+    out << "\n" << o.id << " " << o.bookID << " " << o.libId << " " << o.userID << " " << o.startDay << ";";
     return out;
 }
--- LibraryProject/MalchevskaAnna/Order.h
+++ LibraryProject/MalchevskaAnna/Order.h
@@ -8,16 +8,19 @@ class Order
 private:
     int id;
     int bookID;
+    string libId;
     string userID;
     string startDay;
-
 public:
     Order();
-    Order(int id, int bid, string uid, string sd);
-
+    Order(int id, int bid, string lbId, string uid, string sd);
     int getId() const;
     int getBookID() const;
+    int getOrderId() const;
     string getUserID() const;
+    string getLibId() const;
+    void SetUserID(string userid);
+    void SetLibID(string libID);
     void ConsolePrint()const;
     void ConsoleInput();
     friend istream& operator>>(istream& in, Order& o);
